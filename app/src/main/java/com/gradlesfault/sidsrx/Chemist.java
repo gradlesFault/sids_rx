e4daf0a5ea12f7310960f796cf2a60aab971ef5c
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2017-03-25 16:49:51 +0530

    created Prescription class

---
+++ app/src/main/java/com/gradlesfault/sidsrx/Chemist.java
@@ -0,0 +1,8 @@
+package com.gradlesfault.sidsrx;
+
+/**
+ * Created by Sziraqui on 25-03-2017.
+ */
+
+public class Chemist {
+}
--- app/src/main/java/com/gradlesfault/sidsrx/Prescription.java
+++ app/src/main/java/com/gradlesfault/sidsrx/Prescription.java
@@ -8,4 +8,47 @@ public class Prescription {
     private int ID;
     private boolean EXPIRED = true;
     private int commision = 0;
+    private int doctorID;
+    private int patientID;
+    Prescription(int ID, int doctorID, int patientID){
+        this.ID = ID;
+        this.doctorID = doctorID;
+        this.patientID = patientID;
+        EXPIRED = false;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getCommision() {
+        return commision;
+    }
+
+    public void setCommision(int commision) {
+        this.commision = commision;
+    }
+
+    public int getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(int doctorID) {
+        this.doctorID = doctorID;
+    }
+    public boolean isEXPIRED(){
+        return this.EXPIRED;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
 }
