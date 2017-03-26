d2e0ac5c5da8fbbe4eee4be4886b165b5dd155ba
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2017-03-26 05:35:27 +0530

    Darsh's last commit

--- app/src/main/java/com/gradlesfault/sidsrx/DoctorMainActivity.java
+++ app/src/main/java/com/gradlesfault/sidsrx/DoctorMainActivity.java
@@ -8,6 +8,7 @@ import java.lang.*;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,10 +28,13 @@ import android.widget.Toast;
 public class DoctorMainActivity extends AppCompatActivity {
     Toolbar toolbar;
     Button generate_rx_id_button;
+    Button save_button;
     TextView random_rx;
     EditText patient_id_edittext_doctor;
     Random myRandom;
     SQLiteDatabase db;
+    String paid;
+    String result2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +42,15 @@ public class DoctorMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_main);
         toolbar = (Toolbar) findViewById(R.id.doctor_main_activty_toolbar);
         setSupportActionBar(toolbar);
+        patient_id_edittext_doctor = (EditText) findViewById(R.id.patient_id_edittext_doctor);
+        paid= patient_id_edittext_doctor.getText().toString();
 
         createDatabase();
 
         generate_rx_id_button = (Button) findViewById(R.id.generate_rx_id_button);
-        patient_id_edittext_doctor = (EditText) findViewById(R.id.patient_id_edittext_doctor);
-        random_rx = (TextView) findViewById(R.id.random_rx);
+        save_button = (Button) findViewById(R.id.save_button);
 
+        random_rx = (TextView) findViewById(R.id.random_rx);
 
         generate_rx_id_button.setOnClickListener(new Button.OnClickListener() {
 
@@ -54,24 +60,35 @@ public class DoctorMainActivity extends AppCompatActivity {
                 Random myRandom = new Random();
 
                 result += String.valueOf(myRandom.nextInt());
-                String result2 = result.replaceAll("[-+.^:,]", "");
+               result2 = result.replaceAll("[-+.^:,]", "");
 
                 random_rx.setText(result2);
 
             }
         });
-        insertIntoDB();
+
+        save_button.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+            insertIntoDB();
+
+            }
+        });
     }
     protected void createDatabase(){
         db=openOrCreateDatabase("Random_KEY_db", Context.MODE_PRIVATE, null);
         db.execSQL("CREATE TABLE IF NOT EXISTS Rx_Table(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, RX_key VARCHAR);");
     }
 
+
     protected void insertIntoDB(){
 
-        String query = "INSERT INTO Rx_Table (name) VALUES('result2');";
+        String query = "INSERT INTO Rx_Table (RX_key) VALUES('"+result2+"');";
         db.execSQL(query);
         Toast.makeText(getApplicationContext(),"Saved Successfully", Toast.LENGTH_LONG).show();
+        Log.i("DB","Inserted into db");
     }
 
 }
